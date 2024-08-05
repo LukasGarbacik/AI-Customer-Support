86b1f4d69e78b674b9e1f8e17e7c96a241b16429
Lukas Garbacik <155199938+LukasGarbacik@users.noreply.github.com>
2024-08-05 18:12:54 -0400

    Inital Framework

--- route.js
+++ route.js
@@ -1,4 +1,17 @@
 import {NextResponse} from 'next/sever'
-export default function POST(req){
+import OpenAI from "openai";
+
+export async function POST(req){
+    const openai = new OpenAI();
+    const data = await req.json()
+
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "What is a LLM?"}
+          ],
+        model: "gpt-4o-mini",
+      });
+    console.log(completion.choices[0]);
     return NextResponse.json({message : 'from the server!'})
 }
